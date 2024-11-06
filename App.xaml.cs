abd4d126afceacea15e4f9b795c90baebe55057e
xShir0s <mtodys2006@gmail.com>
2024-11-06 08:53:31 +0100

    dodanie plikow podstawowych

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace cataas
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace cataas
+{
+    public class Image
+    {
+
+    }
+
+    public partial class MainPage : ContentPage
+    {
+        string cat = "https://cataas.com/cat";
+        string gif = "https://cataas.com/cat/gif";
+        string monocat = "https://cataas.com/cat?filter=mono";
+        string negatecat = "https://cataas.com/cat?filter=negate";
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+             
+
+        }
+
+       
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string textcat = "https://cataas.com/cat/says/" + CatText.Text;
+
+            cats.Source = cat;
+            catsGif.Source = gif;
+            catsMono.Source = monocat;
+            catsNegate.Source = negatecat;
+            catsText.Source = textcat;
+
+        }
+    }
+
+}
